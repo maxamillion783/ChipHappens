760a6c0ede66d026a0e950b406dc11f8448b513a
Zoe Cremona <zoshman@mines.edu>
2024-09-24 10:05:12 -0600

    Fixed measurements per card bug

--- Matlab/Sawtooth.m
+++ Matlab/Sawtooth.m
@@ -1,11 +1,12 @@
 % Parameters
+num_cards = 500; % Number of cards we are counting in this data
 samples_per_card = 100;% Number of measurements we will take over one card
-theta = 70;% Angle of the cards [deg]
-t_card = 0.5;% Thickness of the card [in]
+theta = 45;% Angle of the cards [deg]
+t_card = 0.033;% Thickness of the card [in]
 
 % Wave properties derived from parameters
 period = t_card*sind(theta)*tand(theta)+t_card*cosd(theta);% Period [in]
-x = linspace(0, 10*period, floor(100*period));% x-length of waveform we export [in]
+x = linspace(0, num_cards*period, samples_per_card*num_cards);% x-length of waveform we export [in]
 y = zeros(size(x));% storing all of our y points
 duty_cycle = (t_card*sind(theta)*tand(theta))/period;% Percent of the period that is rising
 x_mod = mod(x, period);% x values within a single period
