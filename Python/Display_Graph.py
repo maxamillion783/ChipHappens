43a448992548e996b97f6df83798119a5d3abed1
Deklan Brown <dmbrown@mines.edu>
2024-11-12 13:24:12 -0700

    the python script now display all of the data from the signal

--- Python/Display_Graph.py
+++ Python/Display_Graph.py
@@ -1,10 +1,14 @@
 import matplotlib.pyplot as plt
 import numpy
 
-def pocessCsv(name):
-    with open(name, 'r') as f:
-        data = f.readlines()
-    print(data)
+def processCsv(name):
+    fileStr = "../TestData/"+name;
+    with open(fileStr, 'r') as f:
+        data = [x.strip().split(',') for x in f.readlines()]
+    return [x[:2] for x in data[12:]]
+
 
 if __name__ == "__main__":
-    pass
+    d=processCsv("AP-DATA-006_10-22_trial_3_400mm_1000us_1000Hz_black_solid.csv");
+    plt.plot([float(x[1]) for x in d])
+    plt.show()
