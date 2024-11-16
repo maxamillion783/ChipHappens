fb69217cb84b20d2cc0ac9204e65e4ef4df14914
Flibbertigibbet69 <103669918+Flibbertigibbet69@users.noreply.github.com>
2024-11-16 13:02:30 -0700

    finding double peaks more accurately

--- Python/Cole_Test.py
+++ Python/Cole_Test.py
@@ -10,28 +10,37 @@ import webbrowser
 import scipy as sp
 import numpy as np
 
+display_plots = True
+
 # Load the CSV file into a DataFrame
 #works on these files:
-#file = '../TestData/AP-DATA-016_11-12_WCC_c=60_d=30_t=1000_v=1000_ma=4_messy.csv'
+#file = '../TestData/AP-DATA-010_11-12_BSC_c=78_d=20_t=1000_v=1000_ma=4.csv'
+file = '../TestData/AP-DATA-016_11-12_WCC_c=60_d=30_t=1000_v=1000_ma=4_messy.csv'
+#file = '../TestData/AP-DATA-015_11-12_BSC_c=54_d=30_t=1000_v=1000_ma=4_messy.csv'
+#file = '../TestData/AP-DATA-011_11-12_BSC_c=92_d=10_t=1000_v=1000_ma=4.csv'
 #untested:
-file = '../TestData/AP-DATA-015_11-12_BSC_c=54_d=30_t=1000_v=1000_ma=4_messy.csv'
+
 #does not work:
+#file = '../TestData/AP-DATA-009_11-12_WCC_c=75_d=20_t=1000_v=1000_ma=4.csv'
+
 df = pd.read_csv(file, skiprows = 11)
 
 #find the peaks and troughs of the sensor data using a minimum prominence and distance
-peaks, _ = sp.signal.find_peaks(df["OUT01(mm)"],prominence=0.145,distance=30)
-troughs, _ = sp.signal.find_peaks(1-df["OUT01(mm)"],prominence=0.145)
+#NOTE: prominence = 0.145 works well for 30 degrees, and 
+peaks, _ = sp.signal.find_peaks(df["OUT01(mm)"],prominence=0.125,distance=30)
+troughs, _ = sp.signal.find_peaks(1-df["OUT01(mm)"],prominence=0.125,distance=30)
 #print(peaks)
 
 #plot sensor data as a line plot with peaks and troughs
-fig = px.line(df, y='OUT01(mm)', title='Data Visualization', markers=True) #sensor data
-fig.add_trace(go.Scatter(x=peaks, y=df['OUT01(mm)'][peaks], mode = 'markers', name='Peaks'),) #peaks
-fig.add_trace(go.Scatter(x=troughs, y=df['OUT01(mm)'][troughs], mode = 'markers', name='Troughs'),) #troughs
-fig_filename = 'Sensor Data with Peaks.html'
-fig.update_layout(
-    xaxis_title='Data Number',
-    yaxis_title='Distance (mm)'
-)
+if display_plots:
+    fig = px.line(df, y='OUT01(mm)', title='Data Visualization', markers=False) #sensor data
+    fig.add_trace(go.Scatter(x=peaks, y=df['OUT01(mm)'][peaks], mode = 'markers', name='Peaks'),) #peaks
+    fig.add_trace(go.Scatter(x=troughs, y=df['OUT01(mm)'][troughs], mode = 'markers', name='Troughs'),) #troughs
+    fig_filename = 'Sensor Data with Peaks.html'
+    fig.update_layout(
+        xaxis_title='Data Number',
+        yaxis_title='Distance (mm)'
+    )
 
 #calculate the distance from each peak to its neighbor on the right
 distances_between_peaks = np.zeros((len(peaks)-1))
@@ -56,26 +65,28 @@ peaks_histogram = sp.ndimage.histogram(distances_between_peaks,min_bin,max_bin,n
 troughs_histogram = sp.ndimage.histogram(distances_between_troughs,min_bin,max_bin,num_bins)
 
 #plot peaks histogram
-fig_peaks_histogram = px.bar(x = range(min_bin,max_bin,size_bin), y=peaks_histogram, title='Peaks Histogram')
-fig_peaks_histogram.update_layout(
-    xaxis_title='Quantity in Bin',
-    yaxis_title='Number of Samples Between Peak and its Right Neighbor'
-)
-fig_peaks_histogram_filename = 'Peaks Histogram.html'
-fig_peaks_histogram.write_html(fig_peaks_histogram_filename)
-webbrowser.open(fig_peaks_histogram_filename)
-#fig_peaks_histogram.show()
+if display_plots:
+    fig_peaks_histogram = px.bar(x = range(min_bin,max_bin,size_bin), y=peaks_histogram, title='Peaks Histogram')
+    fig_peaks_histogram.update_layout(
+        xaxis_title='Quantity in Bin',
+        yaxis_title='Number of Samples Between Peak and its Right Neighbor'
+    )
+    fig_peaks_histogram_filename = 'Peaks Histogram.html'
+    fig_peaks_histogram.write_html(fig_peaks_histogram_filename)
+    webbrowser.open(fig_peaks_histogram_filename)
+    #fig_peaks_histogram.show()
 
 #plot troughs histogram
-fig_troughs_histogram = px.bar(x = range(min_bin,max_bin,size_bin), y=troughs_histogram, title='Troughs Histogram')
-fig_troughs_histogram.update_layout(
-    xaxis_title='Quantity in Bin',
-    yaxis_title='Number of Samples Between Trough and its Right Neighbor'
-)
-fig_troughs_histogram_filename = 'Troughs Histogram.html'
-fig_troughs_histogram.write_html(fig_troughs_histogram_filename)
-webbrowser.open(fig_troughs_histogram_filename)
-#fig_troughs_histogram.show()
+if display_plots:
+    fig_troughs_histogram = px.bar(x = range(min_bin,max_bin,size_bin), y=troughs_histogram, title='Troughs Histogram')
+    fig_troughs_histogram.update_layout(
+        xaxis_title='Quantity in Bin',
+        yaxis_title='Number of Samples Between Trough and its Right Neighbor'
+    )
+    fig_troughs_histogram_filename = 'Troughs Histogram.html'
+    fig_troughs_histogram.write_html(fig_troughs_histogram_filename)
+    webbrowser.open(fig_troughs_histogram_filename)
+    #fig_troughs_histogram.show()
 
 #find the most common distance between peaks to figure out how far apart single stacks should be.
 #Anything larger than the cutoff will be considered a double peak.
@@ -98,7 +109,7 @@ expected_peak_average = expected_peaks_sum/num_expected_peaks
 num_expected_troughs = 0
 expected_troughs_sum = 0
 for i in range(0,len(distances_between_troughs)):
-    if distances_between_troughs[i] == single_trough_distance:
+    if distances_between_troughs[i] == single_trough_distance and df.iloc[troughs[i],2] == 'GO':
         num_expected_troughs += 1
         expected_troughs_sum += df['OUT01(mm)'][troughs[i]]
 expected_trough_average = expected_troughs_sum/num_expected_troughs
@@ -107,30 +118,60 @@ trough_cutoff = expected_trough_average - (expected_peak_average - expected_trou
 peak_cutoff = expected_peak_average + (expected_peak_average - expected_trough_average) * 2.5
 
 #add upper and lower bounds to line graph
-fig.add_hline(y=expected_peak_average, line_color = 'green')
-fig.add_hline(y=expected_trough_average, line_color = 'green')
-fig.add_hline(y=peak_cutoff, line_color = 'red')
-fig.add_hline(y=trough_cutoff, line_color = 'red')
+if display_plots:
+    fig.add_hline(y=expected_peak_average, line_color = 'green')
+    fig.add_hline(y=expected_trough_average, line_color = 'green')
+    fig.add_hline(y=peak_cutoff, line_color = 'red')
+    fig.add_hline(y=trough_cutoff, line_color = 'red')
 
-#Set y-axis range to only observe the valid peaks
-fig.update_yaxes(range=[trough_cutoff - 0.5, peak_cutoff + 0.5])
+    #Set y-axis range to only observe the valid peaks
+    fig.update_yaxes(range=[trough_cutoff - 0.3, peak_cutoff + 0.3])
 
-#display line graph
-fig.write_html(fig_filename)
-webbrowser.open(fig_filename)
-#fig.show()
-
-#cound the cards
+#count the cards
 card_count = 0
 num_invalid_peaks = 0
+num_double_peaks = 0
+peak_type = np.zeros(len(peaks),dtype=int) #0=invalid peak, 1=single peak, 2=double peak
 for i in range(0,len(distances_between_peaks)):
     if df['OUT01(mm)'][peaks[i]] < peak_cutoff and df['OUT01(mm)'][peaks[i]] > trough_cutoff:
-        if distances_between_peaks[i] < single_peak_cutoff:
-            card_count+=1
-        else:
+        if df['OUT01(mm)'][peaks[i]] - df['OUT01(mm)'][troughs[i+1]] > 1.3 * (expected_peak_average - expected_trough_average) and distances_between_peaks[i] > single_peak_cutoff:# and df['OUT01(mm)'][peaks[i]] - df['OUT01(mm)'][peaks[i+1]] > 0.5 * (expected_peak_average-expected_trough_average):
             card_count+=2
+            num_double_peaks += 1
+            peak_type[i] = 2
+        else:
+            card_count += 1
+            peak_type[i] = 1
     else:
+        peak_type[i] = 0
         num_invalid_peaks += 1
+peak_type[len(peaks)-1] = 1 #FIXME need a way to tell if the last peak is double or single
 card_count += 1 #FIXME need a way to tell if the last peak is double or single
-print(card_count)
-print(num_invalid_peaks)
+print("Card Count: ", card_count)
+print("Invalid Peaks: ", num_invalid_peaks)
+print("Double Peaks: ", num_double_peaks)
+
+if display_plots:
+    #create arrays of single and double peaks
+    single_peaks = np.zeros(len(peaks)-num_double_peaks-num_invalid_peaks)
+    double_peaks = np.zeros(num_double_peaks)
+    single_peak_idx = 0
+    double_peak_idx = 0
+    for i in range(0,len(peaks)):
+        if peak_type[i] == 1:
+            single_peaks[single_peak_idx] = peaks[i]
+            single_peak_idx += 1
+        elif peak_type[i] == 2:
+            double_peaks[double_peak_idx] = peaks[i]
+            double_peak_idx += 1
+
+    #graph single and double peaks
+    fig.add_trace(go.Scatter(x=single_peaks, y=df['OUT01(mm)'][single_peaks], mode = 'markers', name='Single Peaks', marker_color = 'green')) #peaks
+    fig.add_trace(go.Scatter(x=double_peaks, y=df['OUT01(mm)'][double_peaks], mode = 'markers', name='Double Peaks', marker_color = 'red')) #peaks
+
+    #display line graph
+    fig.write_html(fig_filename)
+    webbrowser.open(fig_filename)
+    #fig.show()
+
+    print(len(peaks))
+    print(len(troughs))
