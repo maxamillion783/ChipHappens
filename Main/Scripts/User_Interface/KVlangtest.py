603da28ad100f4c4fba5ad2e506674ec9419deeb
Deklan Brown <dmbrown@mines.edu>
2025-02-20 10:39:16 -0700

    Added count panel to UI and started working on running total panel with scrollable

--- Main/Scripts/User_Interface/KVlangtest.py
+++ Main/Scripts/User_Interface/KVlangtest.py
@@ -1,3 +1,5 @@
+import sys
+
 from kivy.app import App
 from kivy.uix.boxlayout import BoxLayout
 from kivy.uix.gridlayout import GridLayout
@@ -8,67 +10,188 @@ KV = '''
     GridLayout:
         cols: 2
         rows: 6
-        spacing: 5
+        height: 500
+        width: 500
         padding: 10
+        spacing: 2
         Label:
             text: "Job ID"
             color: (0,0,0,1)
+            font_size: 18
+            size_hint: (0.2, None)
+            height: 36
         TextInput:
             hint_text: ""
             color: (0,0,0,1)
+            font_size: 18
+            size_hint: (0.2, None)
+            height: 36
         Label:
             text: "SubJob ID"
             color: (0,0,0,1)
+            font_size: 18
+            size_hint: (0.2, None)
+            height: 36
         TextInput:
             hint_text: ""
             color: (0,0,0,1)
+            font_size: 18
+            size_hint: (0.2, None)
+            height: 36
         Label:
             text: "Batch ID"
             color: (0,0,0,1)
+            font_size: 18
+            size_hint: (0.2, None)
+            height: 36
         TextInput:
             hint_text: ""
             color: (0,0,0,1)
+            font_size: 18
+            size_hint: (0.2, None)
+            height: 36
         Label:
             text: "Operator ID"
             color: (0,0,0,1)
+            font_size: 18
+            size_hint: (0.2, None)
+            height: 36
         TextInput:
             hint_text: ""
             color: (0,0,0,1)
+            font_size: 18
+            size_hint: (0.2, None)
+            height: 36
         Label:
-            text: "Serial Number"
+            text: "Counter Serial Number"
             color: (0,0,0,1)
+            font_size: 18
+            size_hint: (0.2, None)
+            height: 36
         Label:
             text: "002"
             color: (0,0,0,1)
+            font_size: 18
+            size_hint: (0.2, None)
+            height: 36
         Label:
             text: "Version"
             color: (0,0,0,1)
+            font_size: 18
+            size_hint: (0.2, None)
+            height: 36
         Label:
             text: "V01"
             color: (0,0,0,1)
+            font_size: 18
+            size_hint: (0.2, None)
+            height: 36
 
-<Panel2>:
-    BoxLayout:
-        orientation: 'vertical'
+<CountPanel>:
+    GridLayout:
+        cols: 2
+        rows: 4
+        height: 500
+        width: 500
         padding: 10
-        spacing: 5
+        spacing: 2
         Label:
-            text: "Box Label"
+            text: "Current Count"
+            color: (0,0,0,1)
+            font_size: 18
+            size_hint: (0.2, None)
+            height: 36
         TextInput:
-            hint_text: "Type here"
+            hint_text: ""
+            color: (0,0,0,1)
+            font_size: 18
+            size_hint: (0.2, None)
+            height: 36
+            readonly: True
+            
+        Label:
+            text: "Time Stamp"
+            color: (0,0,0,1)
+            font_size: 18
+            size_hint: (0.2, None)
+            height: 36
+        TextInput:
+            hint_text: ""
+            color: (0,0,0,1)
+            font_size: 18
+            size_hint: (0.2, None)
+            height: 36
+            readonly: True
+        Label:
+            text: "Confidence Interval"
+            color: (0,0,0,1)
+            font_size: 18
+            size_hint: (0.2, None)
+            height: 36
+        TextInput:
+            hint_text: ""
+            color: (0,0,0,1)
+            font_size: 18
+            size_hint: (0.2, None)
+            height: 36
+            readonly: True
         Button:
-            text: "Click Me"
+            text: "Run"
+            background_normal: ''
+            background_color: (1, 0.1, 0.2, 1)
+            # color: (0,1,0,1)
+            size_hint: (0.2, None)
+            height: 50
+            canvas.before:
+                Color: 
+                    rgba: (0,0,0, 1)
+                Line:
+                    width: 2
+                    rectangle: self.x, self.y, self.width, self.height
 
-<Panel3>:
-    AnchorLayout:
-        anchor_x: 'center'
-        anchor_y: 'center'
-        Button:
-            text: "Centered Button"
+
+<TotalPanel>:
+    BoxLayout: 
+        orientation: 'vertical'
+        spacing: 1
+        padding: 10
+        GridLayout:
+            cols: 2
+            rows: 1
+            padding: 10
+            spacing: 2
+            Label:
+                text: "Running Total"
+                color: (0,0,0,1)
+                font_size: 18
+                size_hint: (0.2, None)
+                height: 36
+            TextInput:
+                hint_text: ""
+                color: (0,0,0,1)
+                font_size: 18
+                size_hint: (0.2, None)
+                height: 36
+                readonly: True
+        GridLayout:
+            cols: 1
+            padding: 10
+            spacing: 2
+            ScrollView:
+                do_scroll_x: False
+                do_scroll_y: True
+                Label:
+                    text: 
+                        "Scroll\\n" * 100
+                    color: (0,0,0,1)
+                    font_size: 18
+                    size_hint: (0.2, None)
+                    height: 36
+
 
 BoxLayout:
     orientation: 'vertical'
-    spacing: 10
+    spacing: 1
     padding: 10
     canvas.before:
         Color:
@@ -80,7 +203,7 @@ BoxLayout:
     JobInfoPanel:
         canvas.before:
             Color:
-                rgb: (1,1,1,1)  # Red background
+                rgb: (1,1,1,1)
             Rectangle:
                 pos: self.pos
                 size: self.size
@@ -90,18 +213,23 @@ BoxLayout:
                 width:2
                 rectangle: (self.x, self.y, self.width, self.height)
 
-    Panel2:
+    CountPanel:
         canvas.before:
             Color:
-                rgb: (0.3, 0.8, 0.3)  # Green background
+                rgb: (1,1,1,1)
             Rectangle:
                 pos: self.pos
                 size: self.size
+            Color:
+                rgb: (0,0,0)
+            Line:
+                width:2
+                rectangle: (self.x, self.y, self.width, self.height)
 
-    Panel3:
+    TotalPanel:
         canvas.before:
             Color:
-                rgb: (0.3, 0.3, 0.8)  # Blue background
+                rgb: (1,1,1,1)
             Rectangle:
                 pos: self.pos
                 size: self.size
@@ -110,10 +238,10 @@ BoxLayout:
 class JobInfoPanel(BoxLayout):
     pass
 
-class Panel2(BoxLayout):
+class CountPanel(BoxLayout):
     pass
 
-class Panel3(BoxLayout):
+class TotalPanel(BoxLayout):
     pass
 
 class MyApp(App):
