8c73f7d8bbe8ad54a376063454e649a604de17cc
Zoe Cremona <zoshman@mines.edu>
2025-02-27 10:20:00 -0700

    Changed this to artificially create a sensorDataQuality array. Additionally stopped skipping rows of the csv file since it is formatted differently.

--- Main/Scripts/CSV_Interface/read.py
+++ Main/Scripts/CSV_Interface/read.py
@@ -8,9 +8,16 @@ import pandas as pd
 @return sensorDataQuality array of sensor data qualities (either ALARM or GO for every data point)
 """
 def read(file, returnJourney=False):
-    df = pd.read_csv(file, skiprows = 11)
-    sensorData = df["OUT01(mm)"]
-    sensorDataQuality = df.iloc[:,2]
+    df = pd.read_csv(file) # No longer skipping rows as .csv file format has changed
+    sensorData = df.iloc[:,1]
+    
+    # Iterate over sensorData array to make a sensorDataQuality Array
+    sensorDataQuality = ['']*len(sensorData) # Preallocate string array for sensorDataQuality that is the same size as sensorData
+    for i, dataPoint in enumerate(sensorData, start=0):
+        if dataPoint == -99999: # Note that data is in microns now
+            sensorDataQuality[i] = "ALARM"
+        else:
+            sensorDataQuality[i] = "GO"
     
     if returnJourney:
         sensorData.iloc[::-1].reset_index(drop=True)
