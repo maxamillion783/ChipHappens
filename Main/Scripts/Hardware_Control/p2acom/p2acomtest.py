0ae6ad71ded6385b4b9e65ee1ffca3b6d82b4e57
Flibbertigibbet69 <colebarton5028@gmail.com>
2025-03-11 08:56:35 -0700

    created open serial method

--- Main/Scripts/Hardware_Control/p2acom/p2acomtest.py
+++ Main/Scripts/Hardware_Control/p2acom/p2acomtest.py
@@ -1,8 +1,17 @@
 import serial
 import serial.tools.list_ports
 
+def open_serial():
+    try:
+        ports = serial.tools.list_ports.grep('Arduino')
+        port = next(ports)
+        ser = serial.Serial(port=port, baudrate=9600, timeout=0.1)
+        print(f"Serial port {ser.name} opened successfully")
+        return ser
+    except serial.SerialException as e:
+        print(f"Error opening serial port: {e}")
+        exit()
+
 if __name__ == '__main__':
-    ports = serial.tools.list_ports.grep('Arduino')
-    p = next(ports)
-    print(p.device)
+    open_serial()
 # ###############################################################################
